a26d6728b434e067d34aa013264b2c9aece78813
syeda007 <syedanaazimaunnisa007@gmail.com>
2026-08-02 20:02:47 +0530

    Updated leastnumber program

--- c++ basics/leastnumber.cpp
+++ c++ basics/leastnumber.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
 using namespace std;
-int main()
+int main(){
+    int x;
+    int y;
+    int z;
+    cout<<"enter first number:";
+    cin>>x;
+    cout<<"enter second number:";
+    cin>>y;
+    cout<<"enter third number:";
+    cin>>z;
+    if(x<y and x<z){
+        cout<<"the least number is:"<<x;
+    }
+
+    else if(y<z and y<x){
+        cout<<"the least number is:"<<y;
+
+    }
+
+    else{
+        cout<<"the least number is:"<<z;
+    }
+}
